87f25a258b0a5aeda39a50774482e63bd0e7d558
Abrax <aderounmuabrax@gmail.com>
2024-06-21 13:15:28 +0100

    add requirements

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 import pickle
 import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, accuracy_score
+from sklearn.linear_model import LogisticRegression
 
 #import model 
 pickle_in = open("model.pkl","rb" )
